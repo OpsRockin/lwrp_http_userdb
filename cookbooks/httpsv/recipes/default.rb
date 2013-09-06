156191a484a5e48c0f7affa347902007e68dbe3a
sawanoboly <sawanoboriyu@higanworks.com>
2013-09-06 15:37:20 +0900

    initial

---
+++ cookbooks/httpsv/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: httpsv
+# Recipe:: default
+#
+# Copyright 2013, HiganWorks LLC
+#
+# All rights reserved - Do Not Redistribute
+#
---
+++ solo.rb
@@ -0,0 +1,3 @@
+cookbook_path [File.expand_path('../cookbooks', __FILE__)]
+file_backup_path File.expand_path('../backup', __FILE__)
+file_cache_path File.expand_path('../cache', __FILE__)
