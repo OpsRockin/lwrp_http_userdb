90b52a5d4ee2470f23cf5cf166f52912e397af19
sawanoboly <sawanoboriyu@higanworks.com>
2013-09-06 18:58:36 +0900

    create .htpasswd 1

--- cookbooks/httpsv/providers/auth_basic.rb
+++ cookbooks/httpsv/providers/auth_basic.rb
@@ -1,4 +1,10 @@
+require 'webrick'
+
 action :create do
-  ::FileUtils.touch(@new_resource.name)
+  Chef::Log.warn "====== Create http_auth user #{@new_resource.user}"
+
+  htpasswd = WEBrick::HTTPAuth::Htpasswd.new(@new_resource.name)
+  htpasswd.set_passwd nil, @new_resource.user, @new_resource.password
+  htpasswd.flush
   @new_resource.updated_by_last_action(true)
 end
