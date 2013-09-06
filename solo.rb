112dafb02436de4783d6ce6cd8ef47cf62699b8b
sawanoboly <sawanoboriyu@higanworks.com>
2013-09-06 18:32:14 +0900

    sample1 updated_by_last_action

--- solo.rb
+++ solo.rb
@@ -1,3 +1,4 @@
 cookbook_path [File.expand_path('../cookbooks', __FILE__)]
 file_backup_path File.expand_path('../backup', __FILE__)
 file_cache_path File.expand_path('../cache', __FILE__)
+checksum_path File.expand_path('../checksum', __FILE__)
