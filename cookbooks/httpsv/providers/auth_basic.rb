33eaffdc404736f89538cd1a3d9dde56fbe7f070
sawanoboly <sawanoboriyu@higanworks.com>
2013-09-06 19:44:18 +0900

    use_normal_resource

--- cookbooks/httpsv/providers/auth_basic.rb
+++ cookbooks/httpsv/providers/auth_basic.rb
@@ -9,5 +9,10 @@ action :create do
     htpasswd = WEBrick::HTTPAuth::Htpasswd.new(@new_resource.name)
     htpasswd.set_passwd nil, @new_resource.user, @new_resource.password
     htpasswd.flush
+
+    file @new_resource.name do
+      owner 'guest'
+      mode  0644
+    end
   end
 end
