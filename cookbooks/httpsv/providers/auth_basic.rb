55d0b4ca853c4e423d763d9c50cbd62f72830952
sawanoboly <sawanoboriyu@higanworks.com>
2013-09-07 16:29:52 +0900

    add attribute path

--- cookbooks/httpsv/providers/auth_basic.rb
+++ cookbooks/httpsv/providers/auth_basic.rb
@@ -6,13 +6,8 @@ end
 
 action :create do
   converge_by("====== Create http_auth user #{@new_resource.user}") do
-    htpasswd = WEBrick::HTTPAuth::Htpasswd.new(@new_resource.name)
+    htpasswd = WEBrick::HTTPAuth::Htpasswd.new(@new_resource.path)
     htpasswd.set_passwd nil, @new_resource.user, @new_resource.password
     htpasswd.flush
-
-    file @new_resource.name do
-      owner 'guest'
-      mode  0644
-    end
   end
 end
--- cookbooks/httpsv/resources/auth_basic.rb
+++ cookbooks/httpsv/resources/auth_basic.rb
@@ -3,4 +3,5 @@ default_action :create
 
 attribute :user, :kind_of => String, :required => true
 attribute :password, :kind_of => String
+attribute :path, :kind_of => String, :required => true
 
